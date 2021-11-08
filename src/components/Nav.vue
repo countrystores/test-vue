bc55db8bfc05aec5f2d00bc3f434c18a95729130
gnapoli <gnapoli@wanadev.fr>
2021-11-08 15:26:36 +0100

    routing

---
+++ src/components/Nav.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <nav>
+        <router-link :to="{ name: 'home'}">Accueil</router-link> |
+        <router-link :to="{ name: 'about'}">A propos</router-link>
+    </nav>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1> Acuueil</h1>
+</template>
